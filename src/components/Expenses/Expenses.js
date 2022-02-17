48699be0a0fad8bb8bf6dad1052b75e98c7b12a7
Luc Ho <hotriluc@Lucs-MacBook-Pro.local>
2022-02-17 22:22:22 +0200

    conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,20 +11,25 @@ function Expenses(props) {
     const yearFilterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear)
     }
-    
+
     const filteredExpenses = props.items.filter( (expense) => expense.date.getFullYear() === parseInt(filteredYear))
+
+    let expensesContent = <p>No expenses found</p>
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map( (expense) =>
+        <ExpenseItem
+            key={expense.id}
+            title={expense.title} 
+            amount={expense.amount} 
+            date={expense.date}
+        />)
+    }
 
     return (
         <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onYearFilterChange={yearFilterChangeHandler} />
-            {filteredExpenses.map( (expense) =>
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title} 
-                    amount={expense.amount} 
-                    date={expense.date}
-                />
-            )}
+            {expensesContent}
         </Card>
     )
 }
